cf03cea88cbad6298b2b0a3cd87a7ae3971acd0b
vladimir <vivafa85@mail.ru>
2023-07-12 05:38:49 +0300

    crc8

--- hub.py
+++ hub.py
@@ -15,4 +15,30 @@ def main():
 
 if __name__ == "__main__":
     main()
-рол
+
+def crc8(data):
+    crc = 0
+
+    for byte in data:
+        crc ^= byte
+        for _ in range(8):
+            if crc & 0x80:
+                crc = (crc << 1) ^ 0xD5 # Возможно надо поменять кодировку
+            else:
+                crc <<= 1
+
+    return crc
+
+def check_crc8(payload, checksum):
+    calculated_checksum = crc8(payload)
+    return calculated_checksum == checksum
+
+# Пример использования:
+def check date():
+    payload = [0x01, 0x02, 0x03]
+    crc_8 = 0xFA
+
+    if check_crc8(payload, crc_8):
+        print("Контрольная сумма корректна.")
+    else:
+        print("Контрольная сумма некорректна.") # Надо отправить запрос на повторное отправление данных
